d2eaae6a2b2a36a15224396f35c77a1957dbbbd7
Vladyslav <mvi110601@gmail.com>
2025-09-26 14:25:35 +0300

    updated roots

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [MainController::class, 'index'])->name('home');
+
+
+Route::get('/secretRoom', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/secretRoom', [LoginController::class, 'login']);
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->prefix('crud')->name('crud.')->group(function () {
+
+    // Главная crud
+    Route::view('/', 'crud.index')->name('index');
+
+    // CRUD для категорий
+    Route::resource('books', CrudBookController::class);
+
+    Route::resource('authors', CrudAuthorController::class);
+
 });
