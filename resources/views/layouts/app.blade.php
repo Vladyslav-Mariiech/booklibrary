cb015658187f70168217992c42f10555ba91cb85
Vladyslav <mvi110601@gmail.com>
2025-09-29 06:09:36 +0300

    upd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,42 +15,24 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <div id="app">
-        <nav class="navigation">
-            <a class="navigation-logo"></a>
-            <div class="navigation-list">
+<div id="app">
+    <nav class="navigation">
+        <div class="navigation-container">
+            <a href="/" class="navigation-logo">BookLibrary</a>
+            <div class="navigation-content">
                 <!-- Left Side Of Navbar -->
-                <ul class="navigation-leftSide">
-                    <li><a href="">Books</a></li>
-                    <li><a href="">Authors</a></li>
+                <ul class="navigation-left">
+                    <li><a href="{{ route('books.index') }}">Books</a></li>
                 </ul>
                 <!-- Right Side Of Navbar -->
-                <ul class="navigation-rightSide">
-                    <!-- Authentication Links -->
-                    @guest
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
+                <ul class="navigation-right">
                 </ul>
             </div>
-        </nav>
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+        </div>
+    </nav>
+    <main class="main-content">
+        @yield('content')
+    </main>
+</div>
 </body>
 </html>
