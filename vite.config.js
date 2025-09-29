a7b46e767173e16ac16a42d57761f66b287ce78c
Vladyslav <mvi110601@gmail.com>
2025-09-29 06:12:22 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/crud/books.js'
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host: 'localhost', // или 127.0.0.1
+        port: 5173,
+        strictPort: true,
+        cors: true,
+    },
 });
